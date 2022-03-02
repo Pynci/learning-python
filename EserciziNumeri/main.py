a2c633516ff439b58329d04d173411d5af3930a5
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-03-02 20:19:35 +0100

    Primi esercizietti stupidissimi sui numeri (intero, float, operazioni tra numeri). Ricorda Luca del futuro: mai essere talmente arrogante da pensare di conoscere tutto così bene da non approfondirlo, vale anche per le cose che credi di conoscere già.

---
+++ EserciziNumeri/main.py
@@ -0,0 +1,9 @@
+# 2-8: Scrivi ciascuna delle operazioni tra numeri in modo che il risultato sia 8
+print(5+3)      # addizione
+print(10-2)     # sottrazione
+print(2*4)      # prodotto
+print(16/2)     # divisione
+print(2**3)     # potenza
+
+numeroPreferito = 34
+print(f"\nIl mio numero preferito è {numeroPreferito}")
